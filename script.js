96066da3c0a62de5cd2dccf81a761561871c468e
dennissun1 <dsun96@gmail.com>
2017-04-04 14:55:48 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function main() {
+    document.getElementById("randomSegment").innerHTML = ("This changes the html document itself<br><br><button>Click2</button>");
+    
+    //document.write("This was written in script.js");
+    //document.write("Test");
+    
+}
+
+function display() {
+    document.write("This was written in script.js");
+}
